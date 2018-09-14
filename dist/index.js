3d4ae453a58bd4b0bf298ae334681b3dcda0a242
elamahpla <theamazingness@gmail.com>
2018-09-13 19:01:54 -0500

    Changing bubbles/circle to ES6.

--- dist/index.js
+++ dist/index.js
@@ -16,80 +16,63 @@ const closeSlide = document.getElementById('closeSlideMenu').addEventListener('c
   slide.style.width = '0';
 });
 
-// var bubbles = document.getElementById("bubbles"),
-//   screenWidth = window.innerWidth,
-//   screenHeight = window.innerHeight,
-//   containerWidth = 100,
-//   marginWidth = (screenWidth - containerWidth - 100) / 2;
-// (totalCircles = 4000), (svgNS = "http://www.w3.org/2000/svg");
+const bubbles = document.getElementById('bubbles'),
+  screenWidth = window.innerWidth,
+  screenHeight = window.innerHeight,
+  containerWidth = 100,
+  marginWidth = (screenWidth - containerWidth - 100) / 2;
+(totalCircles = 4000), (svgNS = "http://www.w3.org/2000/svg");
 
-// //Set viewBox for bubble container
-// bubbles.setAttributeNS(null, "viewbox", `0 0 ${screenWidth} ${screenHeight}`);
+const createCircle = () => {
+  const circleXPos = getRandom(1, screenWidth);
+  const circleYPos = getRandom(1, (1 / Math.log(circleXPos)) * 8000 - screenHeight * 0.1);
+  const circleRadius = getRandom(4, 30);
+  const circle = document.createElementNS(svgNS, "circle");
+  circle.setAttributeNS(null, "cx", circleXPos);
+  circle.setAttributeNS(null, "cy", circleYPos);
+  circle.setAttributeNS(null, "r", circleRadius);
+  bubbles.appendChild(circle);
+}
 
-// // For total number of circles, create circles
-// for (let k = 0; k < totalCircles; k++) {
-//   createCircle();
-// }
+const fillColor = (event) => {
+  event.setAttribute('style', `fill: ${colors[Math.floor(Math.random() * colors.length)]}`)
+}
 
-// //Fill circle with color
-// function createCircle() {
-//   var circleXPos = getRandom(1, screenWidth);
-//   var circleYPos = getRandom(
-//     1,
-//     (1 / Math.log(circleXPos)) * 8000 - screenHeight * 0.1
-//   );
-//   var circleRadius = getRandom(4, 30);
-//   var circle = document.createElementNS(svgNS, "circle");
+const getRandom = (min, max) => {
+  return min + Math.floor(Math.random() * (max - min + 1));
+}
 
-//   var rectArea = screenWidth * screenHeight;
-//   // var ellipseArea = (screenWidth - 100) * (screenHeight - 100) * Math.PI;
-//   // var remainingArea = rectArea - ellipseArea / 4;
 
-//   console.log(rectArea);
+function bubbleHover() {
+  for (var e = document.querySelectorAll("circle"), t = 0; t < e.length; t++) {
+    e[t].addEventListener("mouseover", function () {
+      fillColor(this);
+    });
+  }
+}
 
-//   circle.setAttributeNS(null, "cx", circleXPos);
-//   circle.setAttributeNS(null, "cy", circleYPos);
-//   circle.setAttributeNS(null, "r", circleRadius);
+bubbles.setAttributeNS(null, "viewbox", `0 0 ${screenWidth} ${screenHeight}`);
 
-//   bubbles.appendChild(circle);
-// }
+for (let k = 0; k < totalCircles; k++) {
+  createCircle();
+}
 
-// // Get Random Integer
-// function getRandom(min, max) {
-//   return min + Math.floor(Math.random() * (max - min + 1));
-// }
+const colors = [
+  "#a7c554",
+  "#bb523d",
+  "#cc7926",
+  "#da21b1",
+  "#4c528a",
+  "#be1ea4",
+  "#20079b",
+  "#7490a7",
+  "#770e31",
+  "#a52a2a",
+  "#663399",
+  "#8b4513",
+  "#ffc0cb",
+  "#b0e0e6",
+  "#d87093"
+];
 
-// var colors = [
-//   "#a7c554",
-//   "#bb523d",
-//   "#cc7926",
-//   "#da21b1",
-//   "#4c528a",
-//   "#be1ea4",
-//   "#20079b",
-//   "#7490a7",
-//   "#770e31",
-//   "#a52a2a",
-//   "#663399",
-//   "#8b4513",
-//   "#ffc0cb",
-//   "#b0e0e6",
-//   "#d87093"
-// ];
-
-// function fillColor(e) {
-//   e.setAttribute(
-//     "style",
-//     "fill:" + colors[Math.floor(Math.random() * colors.length)]
-//   );
-// }
-
-// function bubbleHover() {
-//   for (var e = document.querySelectorAll("circle"), t = 0; t < e.length; t++) {
-//     e[t].addEventListener("mouseover", function () {
-//       fillColor(this);
-//     });
-//   }
-// }
-
-// bubbleHover();
+bubbleHover();
